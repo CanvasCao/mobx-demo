606a560188488e60bb1759c441153fb0dcb05449
Cao Yuhao <1174478646@qq.com>
2019-07-15 00:22:29 +0800

    add observable array

--- src/index.js
+++ src/index.js
@@ -2,12 +2,33 @@ require('babel-polyfill')
 
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
-import {observable} from 'mobx'
+import {observable, computed} from 'mobx'
 import App from './pages/App'
 
-const mountNode = document.getElementById('MainApp')
-const Root = (<App/>)
+class Temp {
+  id = Math.random();
+  @observable unit = "C";
+  @observable tempC = 25;
+
+  @computed get tempK() {
+    return this.tempC * (9 / 5) + 32
+  };
+
+  @computed get temp() {
+    if (this.unit === "C") {
+      return this.tempC + "C"
+    } else {
+      return this.tempK + "K"
+    }
+  };
+}
+
+const temps = observable([])
+temps.push(new Temp())
+window.temps = temps
+window.Temp = Temp
 
 ReactDOM.render(
-  Root, mountNode
+  <App temps={temps} />,
+  document.getElementById('MainApp')
 )
--- src/pages/App.js
+++ src/pages/App.js
@@ -1,32 +1,16 @@
 import * as React from 'react'
-import {observable} from 'mobx'
+import {observable, computed} from 'mobx'
 import {observer} from 'mobx-react'
-
-const appState = observable({count: 0})
-appState.increment = function () {
-  console.log(this)
-  this.count++
-}
-appState.decrement = function () {
-  this.count--
-}
 
 @observer
 class App extends React.PureComponent {
-  handleIncClick = () => {
-    appState.increment()
-  }
-
-  handleDecClick = () => {
-    appState.decrement()
-  }
 
   render() {
     return (
       <>
-        Counter:{appState.count}
-        <button onClick={this.handleIncClick}>+</button>
-        <button onClick={this.handleDecClick}>-</button>
+        {this.props.temps.map((temp) => {
+          return <div>{temp.temp}</div>
+        })}
       </>
     )
   }
